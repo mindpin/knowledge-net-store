86a2b197057b93b4d20fc6e32a6b19cb16a653b5
fushang318 <fushang318@gmail.com>
2014-05-21 12:20:31 +0800

    add json

--- lib/knowledge-net-store/models/net.rb
+++ lib/knowledge-net-store/models/net.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module KnowledgeNetStore
   class Net
     include Mongoid::Document
@@ -9,5 +11,40 @@ module KnowledgeNetStore
     validates :name, :presence => true, :uniqueness => true
 
     has_many :points, :class_name => 'KnowledgeNetStore::Point'
+
+    def to_json
+      points_hash = []
+      edges_hash  = []
+
+      self.points.each do |point|
+        point_hash = {:id => point.id, :name => point.name, :desc => point.desc}
+        points_hash << point_hash
+        point.parents.each do |parent|
+          edge_hash = {:parent => parent.id, :child => point.id}
+          edges_hash << edge_hash
+        end
+      end
+
+      {
+        "points" => points_hash,
+        "edges"  => edges_hash
+      }.to_json
+    end
+
+    def self.from_json(name, desc, json)
+      net = self.create!(:name => name, :desc => desc)
+      hash = JSON.parse(json)
+      id_point_hash = {}
+      hash["points"].each do |point_hash|
+        point = net.points.create!(:name => point_hash["name"], :desc => point_hash["desc"])
+        id_point_hash[point_hash["id"]] = point
+      end
+      hash["edges"].each do |edge_hash|
+        parent = id_point_hash[edge_hash["parent"]]
+        child  = id_point_hash[edge_hash["child"]]
+        child.parents << parent
+      end
+      net
+    end
   end
 end
--- spec/net_spec.rb
+++ spec/net_spec.rb
@@ -9,4 +9,15 @@ describe KnowledgeNetStore::Net do
     net.name.should == "js"
     net.desc.should == "javascript 教程"
   }
+
+  it{
+    json = IO.read(File.expand_path("../js.json",__FILE__))
+    KnowledgeNetStore::Net.from_json("js","js blanblan", json)
+    KnowledgeNetStore::Net.count.should == 1
+    KnowledgeNetStore::Point.count.should == 93
+
+    net = KnowledgeNetStore::Net.last
+    json = net.to_json
+    json.should_not == ""
+  }
 end
