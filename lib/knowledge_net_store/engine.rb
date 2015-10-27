c9e89ee3fcd9d3a08043bec942543c9bfc826e99
fushang318 <fushang318@gmail.com>
2015-10-27 15:23:50 +0800

    add engine 打补丁机制

--- lib/knowledge_net_store/engine.rb
+++ lib/knowledge_net_store/engine.rb
@@ -3,6 +3,11 @@ module KnowledgeNetStore
     isolate_namespace KnowledgeNetStore
     config.to_prepare do
       ApplicationController.helper ::ApplicationHelper
+
+      Dir.glob(Rails.root + "app/decorators/knowledge_net_store/**/*_decorator.rb").each do |c|
+        require_dependency(c)
+      end
+
     end
   end
 end
